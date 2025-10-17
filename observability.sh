8bffeb20ccd4fbc5cbe10b634f46c6344ef73c67
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-17 18:38:25 +0300

    Metrics hooray!

--- observability.sh
+++ observability.sh
@@ -1,4 +1,15 @@
+# Create namespace for prometheus
 kubectl create namespace monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
+
+# Install only node-exporter and prometheus.
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --set grafana.enabled=false \
+  --set alertmanager.enabled=false \
+  --set kubeStateMetrics.enabled=false \
+  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
+  --set prometheusAdapter.enabled=false \
+  --set nodeExporter.enabled=true
