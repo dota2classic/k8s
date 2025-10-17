2c11c597d6b86fc38969390d532df6d1a22a6c1d
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-17 16:03:24 +0300

    add observability.sh

---
+++ observability.sh
@@ -0,0 +1,4 @@
+kubectl create namespace monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
