558dabc145be4405d249259e88d0b94f45cb7299
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-31 00:46:30 +0300

    Gameservers namespace

--- make-secrets.sh
+++ make-secrets.sh
@@ -1,1 +1,2 @@
 kubectl create secret generic shared-secrets --from-env-file=./secrets/shared.env --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic --namespace gameservers shared-gameserver-secrets --from-env-file=./secrets/gameserver.env --dry-run=client -o yaml | kubectl apply -f -
